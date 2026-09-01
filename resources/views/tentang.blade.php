022acea7b7fea9044efcb6c121a3ceea6784dac4
FalihDzakwan1 <10241028@student.itk.ac.id>
2026-09-01 21:44:37 +0800

    commit tentang

---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Tentang Kami</title>
+</head>
+<body>
+    <h1>Kelompok 03</h1>
+    <p>Anggota:</p>
+    <ul>
+        <li>Elsya Nur Aulia Handayani</li>
+        <li>Falih Dzakwan</li>
+        <li>Fatika Rizki Syahada</li>
+        <li>Indriani Anwar</li>
+    </ul>
+</body>
+</html>
