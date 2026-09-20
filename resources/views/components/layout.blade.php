9f208af0a724f2358d5af682b0a4b46e07ad60c4
Fatika <fatikarizki2005@gmail.com>
2026-09-20 18:00:44 +0800

    feat (views)
    pengecekan di layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
 
 <body>
 
-    <nav>
+    <nav class="main-nav">
 
         <div class="text-2xl font-bold mr-auto">
              KampusLMS
@@ -31,7 +31,19 @@
 
     </nav>
 
-    <main>
+    <main class="p-4 md:p-8">
+        @if(session('success'))
+            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+                <span class="block sm:inline">{{ session('success') }}</span>
+            </div>
+        @endif
+
+        @if(session('error'))
+            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+                <span class="block sm:inline">{{ session('error') }}</span>
+            </div>
+        @endif
+
         {{ $slot }}
     </main>
 
